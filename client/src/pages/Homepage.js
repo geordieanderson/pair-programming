0852fdf974fa000c84366192f2d0db1f3f600888
geordieanderson <geordiea@hotmail.com>
2021-03-12 15:52:34 -0500

    Homepage updates

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,14 +1,22 @@
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Homepage.scss";
 
 
 const Homepage = () => {
     return (
-        <div>
-            <h1>CONNECT 4</h1>
+        <div className="home startdiv">
+            <h1 className="home__title italic">CONNECT 4</h1>
+            <div class="wrapper">
+                <div class="chip"></div>
+                <div class="chip"></div>  
+                <div class="chip"></div>  
+                <div class="chip"></div>  
+            </div>
             <Link to='/playerone'>
-              <button className="button" type="button">CLICK HERE TO BEGIN</button>
-              </Link>
+            <button className="home__button" type="button">CLICK HERE TO BEGIN</button>
+            </Link>
         </div>
     );
 };
